ab1d7417dad1ed73463f5cc61b711cc880d069d8
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2023-02-01 15:06:16 +0100

    wip(ci/cd): archive screenshots

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,6 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('http://angular.traefik.me');
+    cy.screenshot('first-page');
   })
 })
