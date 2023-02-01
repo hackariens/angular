e1281cf8f318304be0f3133595d36aa62a317de1
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2023-02-01 15:11:19 +0100

    wip(ci/cd): test visi angular.traefik.me

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('http://angular.traefik.me');
+    cy.visit('https://angular.traefik.me');
     cy.screenshot('first-page');
   })
 })
